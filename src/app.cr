2ba7ac4d44c88c101203d588405cf58fbb3e4fef
Chris Dosé <chris.dose@gmail.com>
2017-07-08 12:50:35 -0700

    Switch to new GL library

--- src/app.cr
+++ src/app.cr
@@ -1,4 +1,4 @@
-require "./gl"
+require "gl"
 require "./glm"
 require "./window"
 require "./shader_program"
@@ -39,16 +39,16 @@ class App
   end
 
   private def setup
-    GL.clear_color(0.6_f32, 0.77_f32, 0.95_f32, 1.0_f32)
-    GL.enable(GL::DEPTH_TEST)
-    GL.enable(GL::CULL_FACE)
+    GL.clear_color(GL::Color.new(0.2, 0.3, 0.5, 1.0))
+    GL.enable(GL::Capability::DepthTest)
+    GL.enable(GL::Capability::CullFace)
 
     projection = GLM.perspective(45.0_f32, (800.0 / 600.0).to_f32, 0.1_f32, 1000.0_f32)
     @shader_program.set_uniform_matrix_4f("projection", projection)
   end
 
   private def clear
-    GL.clear(GL::COLOR_BUFFER_BIT | GL::DEPTH_BUFFER_BIT)
+    GL.clear(GL::BufferBit::Color | GL::BufferBit::Depth)
   end
 
   private def render
